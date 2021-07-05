9d5b6ba1edb852e04f14aa46b73715a57a2a1b97
Facundo2210 <74992465+Facundo2210@users.noreply.github.com>
2021-07-05 12:17:09 -0300

    Update constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,5 @@
-export const PATH = "http://localhost:3001/";
-export const ALL_SERVER_DOGS = "http://localhost:3001/dogs";
-export const TEMPERAMENTS = "http://localhost:3001/temperament";
+export const PATH = "https://dogappernproject.herokuapp.com/";
+export const ALL_SERVER_DOGS = "https://dogappernproject.herokuapp.com/dogs";
+export const TEMPERAMENTS = "https://dogappernproject.herokuapp.com/temperament";
 export const URL_API_BY_RAZA_NAME =
   "https://api.thedogapi.com/v1/breeds/search?q=";
